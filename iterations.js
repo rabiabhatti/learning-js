efcc40bc8087dfd91bdc231082c08ad035a37094
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-08-18 05:23:44 +0500

    arrayCopyWithin added

--- iterations.js
+++ iterations.js
@@ -71,3 +71,49 @@ const keys = new ArrayKeys(arr)
 // for (const val of keys) {
 //     console.log(val);
 // }
+
+function arrayCopyWithin(arr, target, begin, end) {
+    const { length } = arr
+
+    if (target == null) {
+        return arr
+    }
+
+    let newTarget = target
+    let newBegin = begin == null ? 0 : begin
+    let newEnd = end == null ? length - 1 : end
+
+    if (target < 0) {
+        newTarget = length + target
+    }
+
+    if (newBegin < 0) {
+        newBegin = length + newBegin
+    }
+
+    if (newEnd < 0) {
+        newEnd = length + end
+    } else if (newEnd > length) {
+        newEnd = length
+    }
+
+    if (newTarget >= length) {
+        return arr
+    }
+
+    const sliced = arr.slice(newBegin, newEnd)
+    let slicedIndex = 0
+
+    const targetEnd = newTarget + sliced.length
+
+    for (let i = newTarget; i < targetEnd; i += 1) {
+        arr[i] = sliced[slicedIndex]
+        if (slicedIndex < sliced.length) {
+            slicedIndex += 1
+        }
+    }
+    return arr
+}
+
+
+// console.log(arrayCopyWithin(arr, 1, -4))
