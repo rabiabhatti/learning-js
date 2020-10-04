490f3f789b0709e0a066cc2e471bddd8c295e360
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-10-04 23:14:31 +0500

    Update lrucache.js

--- lrucache.js
+++ lrucache.js
@@ -7,58 +7,54 @@ class LRUCache {
 
     write (key, value) {
         if (this.head ===  null) {
-            const node = {
+            this.head = {
                 key,
                 value,
                 head: null,
                 tail: null
             }
-            this.head = node
         } else {
-            const cache = this.head
-            const node = {
-                key,
-                value,
-            }
-            this.head = node
-            cache.head = node
-            this.head.tail = cache
+            this.writeNewNode(key, value)
         }
     }
 
-    read (input) {
+    read (key) {
 
     }
 
-    remove(input) {
-
+    checkForExisting(key) {
+        if (this.head.tail) {
+            let node = this.head
+            do {
+                // console.log('hello', key, node.key)
+                if (key === node.key) {
+                    // console.log('key === node.key',  node.tail.head)
+                    // const prevHead = node.head
+                    // console.log('prevHead', node.head)
+                    node.head = node.tail
+                    node.tail.head = node.head.head
+                    break
+                }
+                node = node.tail
+            } while (node.tail !== null)
+        }
     }
 
-    checkTotalLength () {
-
+    writeNewNode(key, value) {
+        this.checkForExisting(key)
+        const cache = this.head
+        const node = {
+            key,
+            value,
+        }
+        this.head = node
+        cache.head = node
+        this.head.tail = cache
+        this.head.head = node
     }
 
     clear() {
         this.head = null
-    }
-
-    size () {
-        return this.currentLength
-    }
-
-
-
-    *[Symbol.iterator]() {
-        let index = 0;
-        return {
-            next: () => {
-                if (this.currentLength < this.TotalLength) {
-                    return {value: this.cache[index++], done: false}
-                } else {
-                    return {value: undefined, done: true}
-                }
-            }
-        }
     }
 }
 
@@ -67,9 +63,8 @@ const lruCache = new LRUCache(5)
 lruCache.write(0, 'index 0')
 lruCache.write(1, 'index 1')
 lruCache.write(2, 'index 2')
+lruCache.write(2, 'index 2')
 lruCache.write(3, 'index 3')
 
 console.log(lruCache.head)
-// for (const item of lruCache.cache) {
-//     console.log(item)
-// }
+
