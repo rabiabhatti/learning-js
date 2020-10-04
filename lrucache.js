56e8cb768b443f930e1abc4011425c3111444078
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-10-04 20:32:38 +0500

    Update lrucache.js

--- lrucache.js
+++ lrucache.js
@@ -1,48 +1,50 @@
 // LRU cache
 
 class LRUCache {
-    constructor(TotalLength = 5) {
-        this.currentLength = 0
-        this.TotalLength = TotalLength
-        this.cache = []
+    constructor(length = 5) {
+        this.head = null
     }
 
-    write (input) {
-        const index = this.cache.indexOf(input)
-        if (index === -1) {
-            this.checkTotalLength()
+    write (key, value) {
+        if (this.head ===  null) {
+            const node = {
+                key,
+                value,
+                head: null,
+                tail: null
+            }
+            this.head = node
+        } else {
+            const cache = this.head
+            const oldHead = cache.head
+            const oldTail = cache
+            const node = {
+                key,
+                value,
+                head: null,
+                tail: oldHead,
+            }
+            this.head = node
+            // cache.head = node
+            this.head.tail = cache
+            console.log('oldTail', key, this.head)
         }
-        this.remove(input)
-        this.cache.unshift(input)
-        this.currentLength++
     }
 
     read (input) {
-        this.remove(input)
-        this.cache.unshift(input)
-        this.currentLength++
+
     }
 
     remove(input) {
-        const index = this.cache.indexOf(input)
 
-        if (index > -1) {
-            const cacheCopy = this.cache.slice()
-            cacheCopy.splice(index, 1)
-            this.cache = cacheCopy
-            this.currentLength--
-        }
     }
 
     checkTotalLength () {
-        if (this.currentLength >= this.TotalLength) {
-            this.cache.pop()
-        }
+
     }
 
     clear() {
-        this.cache = [];
-        this.currentLength = 0;
+        this.head = null
     }
 
     size () {
@@ -67,19 +69,12 @@ class LRUCache {
 
 
 const lruCache = new LRUCache(5)
-lruCache.write('index 0')
-lruCache.write( 'index 1')
-lruCache.write( 'index 2')
-lruCache.write('index 3')
-lruCache.remove('index 2')
-lruCache.write( 'index 4')
-lruCache.write('index 3')
-lruCache.write('index 5')
-lruCache.write('index 6')
-lruCache.write('index 5')
-lruCache.read('index 4')
+lruCache.write(0, 'index 0')
+lruCache.write(1, 'index 1')
+lruCache.write(2, 'index 2')
+lruCache.write(3, 'index 3')
 
-console.log(lruCache.cache)
-for (const item of lruCache.cache) {
-    console.log(item)
-}
+console.log(lruCache.head)
+// for (const item of lruCache.cache) {
+//     console.log(item)
+// }
