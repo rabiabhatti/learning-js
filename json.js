1d56ee2d19513320b82f55608957c73d3f957ff1
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-10-15 13:31:46 +0500

    json: add spacing section

--- json.js
+++ json.js
@@ -21,6 +21,20 @@ function checkForCircular(obj) {
     }
 }
 
+function getSpacing(space) {
+    let spacing = space ? (typeof space === 'number' ? (space >= 10 ? 10 : space) : space) : null
+
+    let str = ''
+    if (typeof spacing === 'number') {
+        for (let i = 0; i < spacing; i++) {
+            str += " "
+        }
+    } else {
+        str = spacing
+    }
+    return str
+}
+
 function jsonStringify(input, replacer, space) {
     const circular = checkForCircular(input)
     if (circular === "Converting circular structure to JSON") {
@@ -48,15 +62,21 @@ function jsonStringify(input, replacer, space) {
     }
 
     if (Array.isArray(input) || input instanceof Array) {
-        let res = "["
+        let res = space ? `[\n` :  "["
         for (let i = 0, {length} = input; i < length; i++) {
             const value = jsonStringify(input[i])
             if (value === "TypeError: BigInt value can't be serialized in JSON") {
                 return value
             }
-            res += (i ? ', ' : '') + value
+            const str = getSpacing(space)
+            if (str) {
+                res += (i ? `, ${str}` : '') + value
+            } else {
+                res += (i ? ", " : '') + value
+            }
+
         }
-        return res + "]"
+        return res + (space ? `\n]` :  "]")
     }
 
     if ( input instanceof Set || input instanceof Map || input instanceof WeakSet || input instanceof WeakMap) {
@@ -87,13 +107,22 @@ function jsonStringify(input, replacer, space) {
             }
         }
 
-        return '{' + res.join(', ') + '}';
+        const str = getSpacing(space)
+        let result
+        if (str) {
+            result = res.join(`, ${str}`)
+        } else {
+            result = res.join(', ')
+        }
+
+        return (space ? `{\n` :  "{") + result +  (space ? `\n}` :  "}")
     }
 }
 
-// console.log(jsonStringify([new Number(3), new String('false'), new Boolean(false), new Array()]))
+// console.log(jsonStringify([new Number(3), new String('false'), new Boolean(false), new Array()], null, '\n'))
 // console.log(jsonStringify([10, undefined, function(){}, Symbol('')]))
 // console.log(jsonStringify({x: 2n}))
+console.log(jsonStringify({ x: [10, undefined, function(){}, Symbol('')], y: "hello" }, null, 10))
 
 // const circularReference = {otherData: 123};
 // circularReference.myself = circularReference;
