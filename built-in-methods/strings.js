22515dc8c88518f444079e743b59f66e6a43f775
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-12-19 02:44:34 +0500

    Organization

--- built-in-methods/strings.js
+++ built-in-methods/strings.js
@@ -66,6 +66,24 @@ function strSlice(str, index, endIndex) {
     return newStr
 }
 
+// function stringSlice(str, begin, end) {
+//     const { length } = str
+//     let newStr = ''
+
+//     begin = begin ? begin : 0
+//     end = end ? end : length - 1
+//     const startIndex = begin < 0 ? length + begin : begin
+//     const endIndex = end < 0 ? length + end : end
+
+//     const startSlicingFrom = Math.min(length - 1, startIndex)
+//     const endSlicingAt = Math.min(length - 1, endIndex)
+
+//     for (let i = startSlicingFrom; i < endSlicingAt; i++) {
+//         newStr += str[i]
+//     }
+//     return newStr
+// }
+
 console.log('strSlice', strSlice(string, 0, -1))
 
 function strSubstr(str, startIndex, count) {
@@ -83,6 +101,23 @@ function strSubstr(str, startIndex, count) {
 
     return newStr
 }
+
+// function stringSubstr(str, begin, count) {
+//     const { length } = str
+//     let newStr = ''
+
+//     count = count ? count : length -1
+//     const beginIndex = begin < 0 ? length + begin : begin
+
+//     const startFrom = Math.min(length -1, beginIndex)
+
+//     const endAt = Math.min(length - 1, startFrom + count)
+
+//     for (let i = startFrom; i < endAt; i++) {
+//         newStr += str[i]
+//     }
+//     return newStr
+// }
 
 console.log('strSubstr', strSubstr(string, 0, 4))
 
@@ -109,6 +144,31 @@ function strIndexOf(str, substr) {
 
     return -1
 }
+
+// function stringIndexOf(str, substr) {
+//     const {length} = str
+//     let matchStartIndex = null
+//     let matchLength = 0
+
+//     for (let i = 0; i < length; i++) {
+//         if (matchStartIndex == null) {
+//             if (str[i] === substr[0]) {
+//                 matchStartIndex = i
+//                 matchLength++
+//             }
+//         } else if (str[i] === substr[matchLength]) {
+//             matchLength++
+//         } else {
+//             matchStartIndex = null
+//         }
+
+//         if (matchLength === substr.length) {
+//             return matchStartIndex
+//         }
+//     }
+//     return -1
+// }
+
 
 console.log('strIndexOf', strIndexOf(string, 'quick'))
 
@@ -166,6 +226,35 @@ function strTrim(str) {
     return newStr
 }
 
+// function stringTrim(str) {
+//     const { length } = str
+//     let newStr = ''
+
+//     let startIndex = 0
+//     for (let i = 0; i < length; i++) {
+//         if (str[i] === ' ' || str[i] === '\n') {
+
+//         } else {
+//             startIndex = i
+//             break
+//         }
+//     }
+
+//     let i = length - startIndex
+//     let endIndex = length
+//     while (--i) {
+//         if (endIndex === ' ' || endIndex === '\n') {
+
+//         } else {
+//             endIndex = i + 1
+//             break
+//         }
+//     }
+//     newStr += str.slice(startIndex, endIndex)
+//     return newStr
+// }
+
+
 console.log('strTrim', JSON.stringify(strTrim(`   ${string}    `)))
 
 function strTrimStart(str) {
@@ -220,4 +309,22 @@ function strSplit(str, character) {
     return chunks
 }
 
+
+// function stringSplit(str, separator) {
+//     const { length } = str
+//     const chunks = []
+//     let lastChuckIndex = -1
+
+//     for (let i = 0; i <length; i++) {
+//         if (str[i] === separator) {
+//             chunks.push(str.slice(lastChuckIndex + separator.length, i))
+//             lastChuckIndex = i
+//         }
+//     }
+
+//     if (lastChuckIndex !== length - 1) {
+//         chunks.push(str.slice(lastChuckIndex + separator.length, length-1))
+//     }
+//     return chunks
+// }
 console.log('strSplit', strSplit(string, ' '))
